ec8e9038361207919bfe3fe0a61191a45a275cec
Sunday Toluwani Dere <62195195+dereSundayT@users.noreply.github.com>
2025-02-25 23:12:45 +0100

    auth service setup

---
+++ auth/src/index.ts
@@ -0,0 +1,15 @@
+import exprees from 'express';
+import {json} from "body-parser";
+
+const app = exprees();
+app.use(json());
+
+
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there !');
+});
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000 !!!');
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+```
+npm install typescript ts-node-dev express @types/express
+```
+
+```
+tsc --init
+```
+
+```
+
+
+Auth Service
+   - Build Image ``` docker build -t deretoluwani/auth . ```
